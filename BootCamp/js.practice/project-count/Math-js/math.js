c6f9dfab30d5f7961a7a45a41d6797ad4e57bda6
yoniRhaman <142180697+yoniRhaman@users.noreply.github.com>
2024-03-02 23:29:05 +0200

    Add files via upload
    Self-study of all kinds of important topics in js

---
+++ BootCamp/js.practice/project-count/Math-js/math.js
@@ -0,0 +1,4 @@
+Math.round(x)	//Returns x rounded to its nearest integer
+Math.ceil(x)	//Returns x rounded up to its nearest integer
+Math.floor(x)	//Returns x rounded down to its nearest integer
+Math.trunc(x)	//Returns the integer part of x (new in ES6)
